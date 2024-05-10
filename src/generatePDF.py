154f7822c428f13cba4fa9bec209f698c3ab3578
Alexa Amaguaya <nora.west17@gmail.com>
2024-05-09 21:53:43 -0500

    Help :(

--- src/generatePDF.py
+++ src/generatePDF.py
@@ -20,53 +20,52 @@ class PDFWithBackground(FPDF):
         # Configurar la fuente para el pie de página
         self.set_font('Arial', 'I', 8)
         # Número de página
-        self.cell(0, 10, 'Página ' + str(self.page_no()), 0, 0, 'C')
+        self.cell(0, 10, 'Pag' + str(self.page_no()), 0, 0, 'C')
 
 def create_report(year):    
     pdf = PDFWithBackground()
     pdf.set_background('Images/background.png')
 
-    pdf.add_page()
+    # pdf.add_page()
 
-    pdf.set_y(100)
-    pdf.set_font('Courier',style='B',size=57)
-    pdf.cell(0,0,'Report',0,1,'C')
+    # pdf.set_y(100)
+    # pdf.set_font('Courier',style='B',size=57)
+    # pdf.cell(0,0,'Report',0,1,'C')
 
-    pdf.set_y(125)
-    pdf.set_font('Courier',style='B',size=57)
-    pdf.cell(0,0,'  Videogames',0,1,'C')
+    # pdf.set_y(125)
+    # pdf.set_font('Courier',style='B',size=57)
+    # pdf.cell(0,0,'  Videogames',0,1,'C')
 
-    pdf.set_y(150)
-    pdf.set_font('Courier',style='B',size=57)
-    pdf.cell(0,0,'Sales',0,1,'C')
+    # pdf.set_y(150)
+    # pdf.set_font('Courier',style='B',size=57)
+    # pdf.cell(0,0,'Sales',0,1,'C')
 
-    pdf.add_page()
+    # pdf.add_page()
 
-    pdf.set_y(50)
-    pdf.set_font('Courier',style='B',size=27)   # Arial, Times, Courier
-    pdf.cell(0,0,'Videogames Sales',0,1,'C')
+    # pdf.set_y(50)
+    # pdf.set_font('Courier',style='B',size=27)   # Arial, Times, Courier
+    # pdf.cell(0,0,'Videogames Sales',0,1,'C')
 
-    pdf.set_y(75)
-    pdf.set_font('Courier','B',size=20)   # Arial, Times, Courier
-    pdf.cell(0,0,'',0,1,'R')
+    # pdf.set_y(75)
+    # pdf.set_font('Courier','B',size=20)   # Arial, Times, Courier
+    # pdf.cell(0,0,'',0,1,'R')
 
-    pdf.set_y(155)
-    pdf.set_font('Courier',size=15) 
-    pdf.multi_cell(190,6,'This report presents an analysis of video game sales in recent years, exploring trends in the industry and how different factors impact game sales and reception. Key statistics on global video game sales are examined, as well as changes in consumer preferences over time. This analysis provides a clear insight into the dynamic relationship between video game production and its audience, offering relevant information to understand the market and guide future development and marketing strategies.',0,1,'L')
-    pdf.add_page()
-    pdf.set_y(50)
-    pdf.set_font('Courier',style='B',size=27)   # Arial, Times, Courier
-    pdf.cell(0,0,'Videogames Sales by platform',0,1,'C')
-    pdf.image(f'Images/sales_by_platform_year.png',x=10,y=75,w=200,h=150)
-    pdf.add_page()
-    pdf.set_y(45)
-    pdf.set_font('Courier',style='B',size=23)   # Arial, Times, Courier
-    pdf.cell(0,0,f'',0,1,'R')
+    # pdf.set_y(155)
+    # pdf.set_font('Courier',size=15) 
+    # pdf.multi_cell(190,6,'This report presents an analysis of video game sales in recent years, exploring trends in the industry and how different factors impact game sales and reception. Key statistics on global video game sales are examined, as well as changes in consumer preferences over time. This analysis provides a clear insight into the dynamic relationship between video game production and its audience, offering relevant information to understand the market and guide future development and marketing strategies.',0,1,'L')
+    # pdf.add_page()
+    # pdf.set_y(50)
+    # pdf.set_font('Courier',style='B',size=27)   # Arial, Times, Courier
+    # pdf.cell(0,0,'Videogames Sales by platform',0,1,'C')
+    # pdf.image(f'Images/sales_by_platform_year.png',x=10,y=75,w=200,h=150)
+    # pdf.add_page()
+    # pdf.set_y(45)
+    # pdf.set_font('Courier',style='B',size=23)   # Arial, Times, Courier
     
-    pdf.cell(0,0,f'Report year {year}',0,1,'C')
+    # pdf.cell(0,0,f'Report year {year}',0,1,'C')
     
-    pdf.image(f'Images/pie_chart_genres_{year}.png',x=50,y=50,w=120,h=120)
-    pdf.image(f'Images/bar_chart_top_5_{year}.png',x=50,y=160,w=120,h=120)
+    # pdf.image(f'Images/pie_chart_genres_{year}.png',x=50,y=50,w=120,h=120)
+    # pdf.image(f'Images/bar_chart_top_5_{year}.png',x=50,y=160,w=120,h=120)
 
     return pdf
 
