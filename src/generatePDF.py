6cd7a8391351f60798809c6c8c27dcdc1911fc82
Alexa Amaguaya <nora.west17@gmail.com>
2024-05-10 23:25:46 -0500

    Fixed

--- src/generatePDF.py
+++ src/generatePDF.py
@@ -24,7 +24,7 @@ class PDFWithBackground(FPDF):
 
 def create_report(year):    
     pdf = PDFWithBackground()
-    pdf.set_background('C:\Cursos\Python\videoGamesApp\Images\StyleSheet.png')
+    pdf.set_background('Images/StyleSheet.png')
 
     # pdf.add_page()
 
